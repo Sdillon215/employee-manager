dd00383d8b7e90f6008c95e1bbfec53c7eb74e6c
Sean <sdillon215@gmail.com>
2021-09-08 16:43:51 -0700

    seeds file

--- db/db.sql
+++ db/db.sql
@@ -1,3 +1,4 @@
 DROP DATABASE IF EXISTS employee_manager_db;
-CREATE DATABASE employee_manage_db;
+
+CREATE DATABASE employee_manager_db;
 USE employee_manage_db;
--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,7 @@
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS department;
+
 CREATE TABLE department (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
@@ -8,12 +12,13 @@ CREATE TABLE role (
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INTEGER NOT NULL,
-    FOREIGN KEY (department_id) REFERENCES department (id)
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    FOREIGN KEY ()
-)
+    role_id INTEGER NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES role (id)
+);
