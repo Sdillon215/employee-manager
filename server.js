34fd6575780e199c94f419d492469b6a2a329d53
Sean <sdillon215@gmail.com>
2021-09-09 19:55:45 -0700

    allDepartments function added'

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-const db = require('./db/connection');
 const table = require('console.table');
+const db = require('./db/connection');
 
 startPrompt = function () {
     inquirer.prompt({
@@ -20,23 +20,24 @@ startPrompt = function () {
     })
         .then(({ firstPrompt }) => {
             if (firstPrompt === 'View all departments') {
+                allDepartments();
                 console.log(1);
-            } 
+            }
             if (firstPrompt === 'View all roles') {
                 console.log(2);
-            } 
+            }
             if (firstPrompt === 'View all employees') {
                 console.log(3);
-            } 
+            }
             if (firstPrompt === 'Add a department') {
                 console.log(4);
             }
             if (firstPrompt === 'Add a role') {
                 console.log(5);
-            } 
+            }
             if (firstPrompt === 'Add an employee') {
                 console.log(6);
-            } 
+            }
             if (firstPrompt === 'Update employee role') {
                 console.log(7);
             }
@@ -44,4 +45,12 @@ startPrompt = function () {
                 db.end();
             }
         });
+};
+
+
+function allDepartments() {
+    db.query(`SELECT * FROM department`, (err, res) => {
+        console.table(res);
+    });
+    console.log('Made it to function');
 };
